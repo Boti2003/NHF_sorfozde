95cd8c3c3a82355c1cb8aa8b0af8d1544039061e
Natkay Botond <natkay.botond@gmail.com>
2023-11-13 00:51:23 +0100

    Added JTabbedPane

--- nhf/src/StoreFrame.java
+++ nhf/src/StoreFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class StoreFrame extends JFrame {
 
@@ -8,6 +9,18 @@ public class StoreFrame extends JFrame {
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
+        initTables();
+
+    }
+
+    private void initTables() {
+        JTabbedPane pane = new JTabbedPane();
+        JPanel panel = new JPanel();
+        panel.add(new Label("Kiskutya"));
+        JPanel panel2 = new JPanel();
+        pane.addTab("Nagykutya", panel);
+        pane.addTab("Hatalmas kutya", panel2);
+        this.add(pane, BorderLayout.CENTER);
 
     }
 
